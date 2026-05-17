10aab3771bac08ee970833b2e3a17cea64fc7084
yasasmigajanayake2004 <it25100044@my.sliit.lk>
2026-05-17 13:16:39 +0530

    Updated ReviewServlet

--- src/servlet/ReviewServlet.java
+++ src/servlet/ReviewServlet.java
@@ -1,28 +1,2 @@
 package servlet;
 
-import javax.servlet.*;
-import javax.servlet.annotation.*;
-import javax.servlet.http.*;
-import java.io.*;
-
-import model.*;
-import service.ReviewService;
-
-@WebServlet({"/add-review","/update-review","/delete-review"})
-
-public class ReviewServlet extends HttpServlet {
-
-    private ReviewService rs = new ReviewService();
-
-    protected void doPost(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
-
-        User acc = (User) req.getSession().getAttribute("account");
-
-        if (acc == null) {
-            res.sendRedirect("login.jsp");
-            return;
-        }
-
-        String path = req.getServletPath();
-        boolean admin = !acc.getRole().equals("CUSTOMER");
